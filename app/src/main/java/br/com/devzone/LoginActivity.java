97b6a2462b475a84c916cf8b1384284a7185f540
Glbdias <gbdbrito@gmail.com>
2024-04-23 22:55:05 -0300

    Alteração de campos textView para button de entrar.

--- app/src/main/java/br/com/devzone/LoginActivity.java
+++ app/src/main/java/br/com/devzone/LoginActivity.java
@@ -5,19 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView txtRegitrar_se;
+    Button btnRegistrarLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        txtRegitrar_se = findViewById(R.id.txtRegitrar_se);
+        btnRegistrarLogin = findViewById(R.id.btnRegistrarLogin);
 
-        txtRegitrar_se.setOnClickListener(new View.OnClickListener() {
+        btnRegistrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), WelcomeActivity.class);
--- app/src/main/java/br/com/devzone/WelcomeActivity.java
+++ app/src/main/java/br/com/devzone/WelcomeActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    TextView textView3;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +21,9 @@ public class WelcomeActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
-        textView3 = findViewById(R.id.textView3);
+        btnEntrar = findViewById(R.id.btnEntrar);
 
-        textView3.setOnClickListener(new View.OnClickListener() {
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), LoginActivity.class);
